457cdad4b2833de8ebabf495a83123a885c3a84f
Raisul Islam <raisul.me@gmail.com>
2024-11-29 12:43:31 +0600

    Initial commit

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\{LevelSetList, SetList};
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_83,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+    ])
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ]);
