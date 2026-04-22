616871aed58937700325bbe81bc54ee2ef125ce9
rochi88 <raisul.me@gmail.com>
2026-04-23 03:12:09 +0600

    fix icon

--- src/View/Components/Icon.php
+++ src/View/Components/Icon.php
@@ -10,23 +10,29 @@ use Illuminate\View\Component;
 
 class Icon extends Component
 {
-    public string $resolvedName;
+    public string $resolvedName = '';
 
     public function __construct(
-        public string $name,
+        public ?string $name = null,
         public ?string $id = null,
         public ?string $label = null,
         public string $size = 'w-5 h-5',
     ) {
-        $prefix = config('blade-heroicons.prefix', 'heroicon');
+        if ($this->name !== null && $this->name !== '') {
+            $prefix = config('blade-heroicons.prefix', 'heroicon');
 
-        $this->resolvedName = str_starts_with($this->name, $prefix . '-')
-            ? $this->name
-            : $prefix . '-' . $this->name;
+            $this->resolvedName = str_starts_with($this->name, $prefix . '-')
+                ? $this->name
+                : $prefix . '-' . $this->name;
+        }
     }
 
     public function render(): View|Closure|string
     {
+        if ($this->resolvedName === '') {
+            return '';
+        }
+
         return view('tallui::components.icon');
     }
 }
